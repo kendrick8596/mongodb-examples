254149ccb9e4253d23863ddcad025b226217ff67
Kendrick J. Humphrey <kendrick8596@gmail.com>
2024-05-22 11:15:37 -0500

    Node Example 2

---
+++ index.js
@@ -0,0 +1,56 @@
+const { MongoClient } = require("mongodb-legacy");
+const assert = require("assert").strict;
+const dboper = require("./operations");
+
+const url = "mongodb://localhost:27017/";
+const dbName = "nucampsite";
+
+MongoClient.connect(url, {}, (err, client) => {
+  assert.strictEqual(err, undefined);
+  console.log("Connected correctly to server");
+
+  const db = client.db(dbName);
+
+  db.dropCollection("campsites", (err, result) => {
+    assert.strictEqual(err, undefined);
+    console.log("Dropped Collection", result);
+
+    dboper.insertDocument(
+      db,
+      { name: "Breadcrumb Trail Campground", description: "Test" },
+      "campsites",
+      (result) => {
+        console.log("Insert Document:", result.ops);
+
+        dboper.findDocuments(db, "campsites", (docs) => {
+          console.log("Found Documents:", docs);
+
+          dboper.updateDocument(
+            db,
+            { name: "Breadcrumb Trail Campground" },
+            { description: "Updated Test Description" },
+            "campsites",
+            (result) => {
+              console.log("Updated Document Count:", result.modifiedCount);
+
+              dboper.findDocuments(db, "campsites", (docs) => {
+                console.log("Found Documents:", docs);
+
+                dboper.removeDocument(
+                  db,
+                  { name: "Breadcrumb Trail Campground" },
+                  "campsites",
+                  (result) => {
+                    console.log("Deleted Document Count:", result.deletedCount);
+
+                    client.close();
+                  }
+                );
+              });
+            }
+          );
+        });
+      }
+    );
+  });
+});
